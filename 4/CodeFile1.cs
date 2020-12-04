bd94612be75ceead3ec51d5616e4d3d902f1e68d
GDFno1 <74794051+GDFno1@users.noreply.github.com>
2020-12-04 11:27:34 +0200

    Add project files.

---
+++ 4/CodeFile1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class UngaBung
+{
+    static void Main()
+    {
+        int prev = 0;
+        int current = 1;
+        int temp;
+        int Collischestvo;
+        int i = 0;
+
+        Console.WriteLine("Enter a col of fibbonachi numbers!");
+        Collischestvo = Int32.Parse(Console.ReadLine());
+        Console.Clear();
+
+        do
+        {
+            Console.WriteLine(current);
+            temp = current;
+            current = current + prev;
+            prev = temp;
+            i++;
+        }
+
+        while (i < Collischestvo);
+
+        Console.ReadKey();
+
+    }
+}
